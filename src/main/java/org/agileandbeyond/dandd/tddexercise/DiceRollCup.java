7adb84d34db7d276aafeade3347596f265c10ca2
Mike Szmansky <mszmansky@pobox.com>
2016-03-07 23:53:41 -0500

    Feature-27:  Added more features to the dice

--- src/main/java/org/agileandbeyond/dandd/tddexercise/DiceRollCup.java
+++ src/main/java/org/agileandbeyond/dandd/tddexercise/DiceRollCup.java
@@ -3,14 +3,36 @@ package org.agileandbeyond.dandd.tddexercise;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DiceRollCup {
 
 	private Set<Die> diceCup;
+	private int diceCount;
 	private int value;
 	
 	public DiceRollCup() {
 		diceCup = new HashSet<Die>();
+	}
+	
+	public DiceRollCup(String diceNotation) {
+		Pattern pattern = Pattern.compile("(\\d*)(d)(\\d+)");
+		Matcher m = pattern.matcher(diceNotation);
+		int numberOfDice = 1;
+		int sides = 0;
+		if (m.find()) {
+			diceCup = new HashSet<Die>();
+			if (!m.group(1).isEmpty()) {
+				numberOfDice = Integer.valueOf(m.group(1)).intValue();
+			}
+			sides = Integer.valueOf(m.group(3)).intValue();
+			for (int i=0; i<numberOfDice; i++) {
+				addDieToCup(new Die(sides));
+			}
+		} else {
+			throw new IllegalArgumentException("Invalid dice notation");
+		}
 	}
 	
 	public DiceRollCup(List<Die> dice){
@@ -24,10 +46,11 @@ public class DiceRollCup {
 			die.roll();
 			value += die.getValue();
 		}
+		this.diceCount = 0;
 	}
 	
 	public void addDie(Die die) {
-		diceCup.add(die);
+		addDieToCup(die);
 	}
 	
 	public String toString(){
@@ -40,5 +63,14 @@ public class DiceRollCup {
 
 	public int getValue() {
 		return value;
+	}
+
+	public int getDiceCount() {
+		return diceCount;
+	}
+	
+	private void addDieToCup(Die die) {
+		diceCup.add(die);
+		this.diceCount++;
 	}
 }
--- src/test/java/org/agileandbeyond/dandd/tddexercise/DiceTest.java
+++ src/test/java/org/agileandbeyond/dandd/tddexercise/DiceTest.java
@@ -47,6 +47,34 @@ public class DiceTest {
 	}
 	
 	@Test
+	public void shouldHaveThreeDiceInCup() {
+		DiceRollCup diceCup = new DiceRollCup();
+		diceCup.addDie(new Die(6));
+		diceCup.addDie(new Die(6));
+		diceCup.addDie(new Die(6));
+		
+		assertEquals(3, diceCup.getDiceCount());
+	}
+	
+	@Test
+	public void shouldHaveZeroDiceInCupWhenNoDiceAreAddedYet() {
+		DiceRollCup diceCup = new DiceRollCup();
+		
+		assertEquals(0, diceCup.getDiceCount());
+	}
+	
+	@Test
+	public void shouldHaveZeroDiceInCupAfterDiceAreThrown() {
+		DiceRollCup diceCup = new DiceRollCup();
+		diceCup.addDie(new Die(6));
+		diceCup.addDie(new Die(6));
+		diceCup.addDie(new Die(6));
+		diceCup.throwDice();
+		
+		assertEquals(0, diceCup.getDiceCount());
+	}
+	
+	@Test
 	public void shouldPrintDiceRollCupProperlyWithDifferentDie() {
 		DiceRollCup diceCup = new DiceRollCup();
 		diceCup.addDie(new Die(4));
@@ -57,4 +85,51 @@ public class DiceTest {
 		diceCup.throwDice();
 		System.out.println(diceCup.toString());
 	}
+	
+	@Test
+	public void shouldCreateOneSixSidedDieInDiceRollCupUsingDiceNotation() {
+		DiceRollCup diceCup = new DiceRollCup("1d6");
+		assertEquals(1, diceCup.getDiceCount());
+		diceCup.throwDice();
+		assertTrue(diceCup.getValue() >= 1 && diceCup.getValue() <= 6);
+	}
+	
+	@Test
+	public void shouldCreateTwoSixSidedDieInDiceRollCupUsingDiceNotation() {
+		DiceRollCup diceCup = new DiceRollCup("2d6");
+		assertEquals(2, diceCup.getDiceCount());
+		diceCup.throwDice();
+		assertTrue(diceCup.getValue() >= 2 && diceCup.getValue() <= 12);
+	}
+	
+	@Test
+	public void shouldCreateOneTenSidedDieInDiceRollCupUsingDiceNotation() {
+		DiceRollCup diceCup = new DiceRollCup("d10");
+		assertEquals(1, diceCup.getDiceCount());
+		diceCup.throwDice();
+		assertTrue(diceCup.getValue() >= 1 && diceCup.getValue() <= 10);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void shouldThrowIllegalArgumentExceptionWhenNotDiceNotation() {
+		new DiceRollCup("Grog");
+	}
+	
+	@Test
+	public void shouldCreateTwoTenSidedDiceInDiceRollCupUsingDiceNotation() {
+		DiceRollCup diceCup = new DiceRollCup("2d10");
+		assertEquals(2, diceCup.getDiceCount());
+		diceCup.throwDice();
+		assertTrue(diceCup.getValue() >= 2 && diceCup.getValue() <= 20);
+	}
+	
+	@Test
+	public void shouldCreateAValidPercentile() {
+		DiceRollCup diceCup = new DiceRollCup("d100");
+		assertEquals(1, diceCup.getDiceCount());
+		diceCup.throwDice();
+		assertTrue(diceCup.getValue() >= 1 && diceCup.getValue() <= 100);
+		System.out.println(diceCup.toString() + "%");
+	}
+	
 }
